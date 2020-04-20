fdd4e44b1dd30fcf6ee769182beea2604ec4ce3d
Ola Gontarz <aleksandra.m.gontarz@gmail.com>
2020-04-20 22:33:52 +0200

    Set up registering and login routes

---
+++ middleware/auth.js
@@ -0,0 +1,23 @@
+const jwt = require('jsonwebtoken');
+const config = require('config');
+
+const auth = async (req, res, next) => {
+	const token = req.header('x-auth-token');
+	if (!token) {
+		return res.status(401).json({ msg: 'Invalid token' });
+	}
+	try {
+		await jwt.verify(token, config.get('jwtSecret'), (error, decoded) => {
+			if (error) {
+				return res.status(401).json({ msg: 'Invalid token' });
+			}
+			console.log(decoded);
+			req.user = decoded.user;
+			next();
+		});
+	} catch (e) {
+		return res.status(401).json({ msg: 'Need authorization' });
+	}
+};
+
+module.exports = auth;
